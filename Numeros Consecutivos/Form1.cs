07a78d11f6e1f498b574ab0cf9d1be73412b4763
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-13 20:13:26 -0500

    Numeros consecutivos

---
+++ Numeros Consecutivos/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_Consecutivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i;
+            int consecutivo;
+            consecutivo = Convert.ToInt16(textBox1.Text);
+            for (i = 1; i <= consecutivo; i++)
+                listBox1.Items.Add(i);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+        }
+    }
+}
